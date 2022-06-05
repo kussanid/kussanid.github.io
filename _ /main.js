ef1ad486bb56fbc416dcd56da9966a597b031bb7
kussanid <104693108+kussanid@users.noreply.github.com>
2022-06-05 19:28:26 +0300

    Update and rename js/main.js to _ /main.js

---
+++ _ /main.js
@@ -0,0 +1,3 @@
+document.getElementById('mybutton').onclick = function() {
+  window.location.href = 'https://ru.wiktionary.org/wiki/%D0%BF%D0%BE%D1%88%D0%BB%D0%B8_%D0%BD%D0%B0_%D1%85%D1%83%D0%B9';
+};
