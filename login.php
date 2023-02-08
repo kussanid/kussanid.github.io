498347371978db94e62e686db431c32076fcd6b9
kussanid <104693108+kussanid@users.noreply.github.com>
2023-02-09 02:08:26 +0300

    Update login.php

--- login.php
+++ login.php
@@ -20,6 +20,47 @@
       <label for="password">Password:</label>
       <input type="password" name="password" />
       <input type="submit" value="Login" />
+        // In the registration page 
+
+// Add a field for each user to input their username and password 
+
+<form>
+  <label for="username">Username:</label>
+  <input type="text" name="username" id="username" />
+  <label for="password">Password:</label>
+  <input type="password" name="password" id="password" />
+  <input type="submit" value="Register" />
+</form>
+
+// In the backend 
+
+<?php
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+  //check if username and password are valid
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  //Connect to the database
+  $conn = mysqli_connect('localhost', 'db_user', 'db_password', 'db_name');
+
+  //Query the database
+  $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+  $result = mysqli_query($conn, $query);
+
+  //Check if a match was found
+  if(mysqli_num_rows($result) == 1) {
+    //Authorize the user
+    session_start();
+    $_SESSION['username'] = $username;
+    //Redirect the user to the home page
+    header('Location: home.php');
+  } else {
+    //Invalid credentials
+    echo 'Invalid credentials';
+  }
+}
+?>
     </form>
   </div>
     </div>
