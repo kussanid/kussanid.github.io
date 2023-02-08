4340e4c9c51996723e79afa7c9736f1568d32c2f
kussanid <104693108+kussanid@users.noreply.github.com>
2023-02-09 02:06:28 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,6 +2,10 @@
 <head>
     <meta charset="utf-8">
     <title>arbuz</title>
+    <style>
+  body {
+    background-color: pink;
+  }
     <link href="css/style.css" media="screen" rel="stylesheet">
     <link href= 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 </head>
@@ -9,18 +13,18 @@
 <div class="container mlogin">
     <div id="login">
         <h1>Вход</h1>
-        <form action="" id="loginform" method="post"name="loginform">
-            <p><label for="user_login">Имя опльзователя<br>
-                    <input class="input" id="username" name="username"size="20"
-                           type="text" value=""></label></p>
-            <p><label for="user_pass">Пароль<br>
-                    <input class="input" id="password" name="password"size="20"
-                           type="password" value=""></label></p>
-            <p class="submit"><input class="button" name="login"type= "submit" value="Log In"></p>
-            <p class="regtext">Еще не зарегистрированы?<a href= "register.php">Регистрация</a>!</p>
-        </form>
+         <div>
+    <form action="authorize.php" method="post">
+      <label for="username">Username:</label>
+      <input type="text" name="username" />
+      <label for="password">Password:</label>
+      <input type="password" name="password" />
+      <input type="submit" value="Login" />
+    </form>
+  </div>
     </div>
 </div>
+    
 <footer>
 
 
